2cf97e4b93e2d12581f458eb5f77bc5f13f137fd
Vignesh <r.vignesh412@gmail.com>
2017-07-28 15:34:11 +0530

    Login Authentication POC
    Token Based Authentication

---
+++ LoginAuth/LoginAuth/webpack.config.js
@@ -0,0 +1,65 @@
+﻿/*webpack.dev.config.js - to configure dev related bundles*/
+var path = require('path');
+var webpack = require('webpack');
+var WatchTimePlugin = require('webpack-watch-time-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+template: __dirname + '/html/index.html',
+filename: 'index.html',
+inject: 'body'
+})
+module.exports = {
+    entry: {
+        "app": [
+          path.join(__dirname, 'src', 'Main.js')
+        ]
+    },
+    devServer : {
+	port : 8080
+},
+    output: {
+        path: path.join(__dirname, '', 'release'),
+        filename: "[name].js",
+        chunkFilename: "[id].js"
+    },
+    resolve: {
+        alias: {
+          'react': path.join(__dirname, 'node_modules', 'react')
+        },
+        extensions: ['.webpack.js', '.web.js', '.js', '.jsx', '.css', '.png', '.jpg'],
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin("bundle"),
+      WatchTimePlugin,
+      HtmlWebpackPluginConfig
+    ],
+    watch: false,
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'                
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.png$/,
+                loader: "file-loader?name=[path][name].[ext]"
+            },
+            {
+                test: /\.(png|woff|woff2|eot|ttf)$/,
+                loader: 'url-loader?limit=100000'
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            }
+        ]
+    }
+};
---
+++ LoginAuth/LoginAuth/wwwroot/js/component/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+export default class Login extends React.createClass{
+    
+    
+    
+    render(){
+        return (
+            <div>
+                  <input  
+            </div>
+        );
+    }
+}
