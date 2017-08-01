49b0680bb8546a34ed6f3e3ee9f18e6463621a07
Vignesh Ravishankar <vravish@onvia.com>
2017-08-01 10:50:19 +0530

    update

---
+++ LoginAuth/LoginAuth/Authentication/UserIdentity.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace LoginAuth.Authentication
+{
+    public static class UserIdentity
+    {
+ 
+        public static Task<ClaimsIdentity> GetIdentity(string username, string password)
+        {
+           
+            // Don't do this in production, obviously!
+            if (username == "TEST" && password == "TEST123")
+            {
+                return Task.FromResult(new ClaimsIdentity(new GenericIdentity(username, "Token"), new Claim[] { }));
+            }
+
+            // Credentials are invalid, or account doesn't exist
+            return Task.FromResult<ClaimsIdentity>(new ClaimsIdentity(new GenericIdentity(username, "Token"), new Claim[] { }));
+        }
+         
+    }
+}
--- LoginAuth/LoginAuth/Startup.Auth.cs
+++ LoginAuth/LoginAuth/Startup.Auth.cs
@@ -64,7 +64,7 @@ namespace LoginAuth
                 Audience = Configuration.GetSection("TokenAuthentication:Audience").Value,
                 Issuer = Configuration.GetSection("TokenAuthentication:Issuer").Value,
                 SigningCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256),
-                IdentityResolver = GetIdentity
+                IdentityResolver = UserIdentity.GetIdentity
             };
 
             app.UseMiddleware<TokenProviderMiddleware>(Options.Create(tokenProviderOptions));
@@ -72,17 +72,6 @@ namespace LoginAuth
 
         }
 
-        private Task<ClaimsIdentity> GetIdentity(string username, string password)
-        {
-            // Don't do this in production, obviously!
-            if (username == "TEST" && password == "TEST123")
-            {
-                return Task.FromResult(new ClaimsIdentity(new GenericIdentity(username, "Token"), new Claim[] { }));
-            }
-
-            // Credentials are invalid, or account doesn't exist
-            return Task.FromResult<ClaimsIdentity>(null);
-        }
-
+   
     }
 }
